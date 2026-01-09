da49d2dc281b5ad64e8d20944d312b84960ab835
autumn <cyancirrus@gmail.com>
2026-01-09 16:46:47 -0600

    okay works well not in love with naming but isolated enough to be able to work in main

--- src/environment/representation.rs
+++ src/environment/representation.rs
@@ -5,7 +5,7 @@ use std::fs;
 use std::error::Error;
 
 type Coord = (isize, isize);
-enum Object {
+pub enum Object {
     Doorway,
     Obstacle,
     Corner,
@@ -19,46 +19,15 @@ pub struct Bounds {
     max_y: isize,
 }
 
-fn readmap(path:&str) -> Result<Environment, Box<dyn Error>> {
-    let content = match fs::read_to_string(path) {
-        Ok(c) => c,
-        Err(e) => return Err(format!("Unable to read path {path:?}\n{e:?}").into()),
-    };
-    let (mut max_x, mut max_y) = (0, 0);
-    let mut mirrored_objects = Vec::new();
-    for (idx_y, line) in content.lines().enumerate() {
-        for (idx_x, cell) in line.as_bytes().chunks_exact(3).enumerate() {
-            let obj = match cell[1] {
-                b' ' => continue,
-                b'+' => Object::Doorway,
-                b'*' => Object::Obstacle,
-                b'#' => Object::Wall,
-                b'x' => Object::Corner,
-                _ => return Err(format!("Unexpected symbol found in map with source {path:?}").into()),
-            };
-            mirrored_objects.push(((idx_x as isize, idx_y as isize), obj));
-            max_x = max_x.max(idx_x as isize);
-        }
-        max_y = max_y.max(idx_y as isize);
-    }
-    let mut information = HashMap::new();
-    let bounds = Bounds::new(0, 0, max_x + 1, max_y + 1);
-    // Mapping is easiest to think of as direct representation ie mirrored b/c of parsing
-    for ((idx_x, mir_idx_y), obj) in mirrored_objects {
-        information.insert( (idx_x , max_y - mir_idx_y), obj);
-    }
-
-    Ok(Environment { information, bounds })
-}
 
 
 impl Bounds {
-    fn new(min_x:isize, min_y:isize, max_x:isize, max_y:isize) -> Self {
+    pub fn new(min_x:isize, min_y:isize, max_x:isize, max_y:isize) -> Self {
         Self { min_x, min_y, max_x, max_y }
     }
 }
 
-struct Environment {
+pub struct Environment {
     information: HashMap<Coord, Object>,
     bounds: Bounds,
 }
--- src/parser/map.rs
+++ src/parser/map.rs
@@ -1,3 +1,36 @@
-pub fn test() {
-    println!("hello");
+use std::collections::HashMap;
+use std::fs;
+use std::error::Error;
+use crate::environment::representation::{Environment, Bounds, Object};
+
+pub fn readmap(path:&str) -> Result<Environment, Box<dyn Error>> {
+    let content = match fs::read_to_string(path) {
+        Ok(c) => c,
+        Err(e) => return Err(format!("Unable to read path {path:?}\n{e:?}").into()),
+    };
+    let (mut max_x, mut max_y) = (0, 0);
+    let mut mirrored_objects = Vec::new();
+    for (idx_y, line) in content.lines().enumerate() {
+        for (idx_x, cell) in line.as_bytes().chunks_exact(3).enumerate() {
+            let obj = match cell[1] {
+                b' ' => continue,
+                b'+' => Object::Doorway,
+                b'*' => Object::Obstacle,
+                b'#' => Object::Wall,
+                b'x' => Object::Corner,
+                _ => return Err(format!("Unexpected symbol found in map with source {path:?}").into()),
+            };
+            mirrored_objects.push(((idx_x as isize, idx_y as isize), obj));
+            max_x = max_x.max(idx_x as isize);
+        }
+        max_y = max_y.max(idx_y as isize);
+    }
+    let mut information = HashMap::new();
+    let bounds = Bounds::new(0, 0, max_x + 1, max_y + 1);
+    // Mapping is easiest to think of as direct representation ie mirrored b/c of parsing
+    for ((idx_x, mir_idx_y), obj) in mirrored_objects {
+        information.insert( (idx_x , max_y - mir_idx_y), obj);
+    }
+
+    Ok(Environment::new(information, bounds ))
 }
