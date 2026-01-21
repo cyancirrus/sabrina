2353f0b263f7da831713e638fcbacc95aa980cf3
autumn <cyancirrus@gmail.com>
2026-01-20 20:56:17 -0600

    cleaning up remembered lines of thought

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use sabrina::sensor::lidar::Lidar;
 use std::collections::{HashMap, HashSet, BinaryHeap};
 use sabrina::environment::info::reconstruct;
 
-// // TODO: Bit-level Quadtree Fixes (Off-by-one/Masks)
+// // NOTE: Bit-level Quadtree Fixes (Off-by-one/Masks)
 // // Unknown State Initialization + Visual Debugger (Essential for visibility)
 // // Observation Logic (Unknown \(\rightarrow \) Free/Occupied).LU Pivoting (Numerical insurance)
 // // Multi-ray LiDAR & Planner Implementation.
@@ -21,13 +21,18 @@ use sabrina::environment::info::reconstruct;
 // - second if it is at lower level of granularity use the those connected by the edge and ensure that we recurse until we find the nodes which actually exist
 // - lastly implement a*
 
+// TODO: refactor this the double integer scaling just would need a bottom up refactor of entire morton
+// interface
+
 fn point(m_coord: &Coord) -> Coord {
-    // retrieves the centroid scaled by two two prevent half-integers
-    let mask = (1 << PARTITION) - 1;
-    (
-        ((m_coord.0 & mask) << 1) + (1 << (m_coord.0 >> PARTITION)),
-        ((m_coord.1 & mask) << 1) + (1 << (m_coord.1 >> PARTITION)),
-    )
+    // An interface for retrieving purely for retrieving distance
+    // retrieves the centroid scaled by two in order to prevent half-integers
+    let level = m_coord.0 >> PARTITION;
+        let mask = (1 << PARTITION) - 1;
+        (
+            ((m_coord.0 & mask) << 1) + (1 << level ),
+            ((m_coord.1 & mask) << 1) + (1 << level ),
+        )
 }
 fn centroid_estimate(sm_coord: &Coord, tm_coord: &Coord) -> usize {
     // distance between source-centroid and target-centroid
@@ -35,9 +40,9 @@ fn centroid_estimate(sm_coord: &Coord, tm_coord: &Coord) -> usize {
     s_centr.0.abs_diff(t_centr.0) + s_centr.1.abs_diff(t_centr.1)
 }
 
-fn cardinals(m_coord: &Coord) -> [Coord; 4] {
+fn find_cardinals(m_coord: &Coord) -> [Coord; 4] {
     // space is double to avoid halfints on the quadtree for centroids
-    let dh = 1 << ((m_coord.0 >> PARTITION) + 1);
+    let dh = 1 << (m_coord.0 >> PARTITION);
     // clockwise e,n,w,s
     [
         (m_coord.0 + dh, m_coord.1),
@@ -48,59 +53,28 @@ fn cardinals(m_coord: &Coord) -> [Coord; 4] {
 }
 pub fn east_morton(morton: &Coord) -> [Coord; 2] {
     // child filtered east neighbors; dx := 1
-    let level = morton.0 >> PARTITION;
+    let level = (morton.0 >> PARTITION) - 1;
     [
         (
-            (morton.0 - (1 << PARTITION)) | 1 << (level + 1),
-            (morton.1 - (1 << PARTITION)),
+            (morton.0 - (1 << PARTITION)) | 1 << level,
+            (morton.1 - (1 << PARTITION)) ,
         ),
         (
-            (morton.0 - (1 << PARTITION)) | 1 << (level + 1),
+            (morton.0 - (1 << PARTITION)) | 1 << level,
             (morton.1 - (1 << PARTITION)) | 1 << level,
         ),
     ]
 }
-
-// pub fn east_morton(morton: &Coord) -> [Coord; 2] {
-//     // child filtered east neighbors; dx := 1
-//     let level = morton.0 >> PARTITION;
-//     let span = 1 + (1<<level);
-//     let mask = 1<< (level-1);
-//     let point = point(morton);
-//     println!("level {level:?}");
-//     [
-//         (point.0 + span, point.1 - 1 << (level - 1)),
-//         (point.0 + span, point.1 + 1 << (level - 1)),
-//         // (
-//         //     (morton.0 - (1 << PARTITION)) | 1 << level,
-//         //     (morton.1 - (1 << PARTITION)),
-//         // ),
-//         // (
-//         //     (morton.0 - (1 << PARTITION)) | 1 << level,
-//         //     (morton.1 - (1 << PARTITION)) | 1 << level,
-//         // ),
-//     ]
-//     // [
-//     //     (
-//     //         (morton.0 - (1 << PARTITION)) | 1 << level,
-//     //         (morton.1 - (1 << PARTITION)),
-//     //     ),
-//     //     (
-//     //         (morton.0 - (1 << PARTITION)) | 1 << level,
-//     //         (morton.1 - (1 << PARTITION)) | 1 << level,
-//     //     ),
-//     // ]
-// }
 pub fn north_morton(morton: &Coord) -> [Coord; 2] {
     // child filtered west neighbors; dy := 1
     let level = (morton.0 >> PARTITION) - 1;
     [
         (
-            (morton.0 - (1 << PARTITION)),
+            (morton.0 - (1 << PARTITION)) | 1 << level,
             (morton.1 - (1 << PARTITION)) | 1 << level,
         ),
         (
-            (morton.0 - (1 << PARTITION)) | 1 << level,
+            (morton.0 - (1 << PARTITION)),
             (morton.1 - (1 << PARTITION)) | 1 << level,
         ),
     ]
@@ -109,10 +83,13 @@ pub fn west_morton(morton: &Coord) -> [Coord; 2] {
     // child filtered west neighbors; dx := 0
     let level = (morton.0 >> PARTITION) - 1;
     [
-        ((morton.0 - (1 << PARTITION)), (morton.1 - (1 << PARTITION))),
         (
             (morton.0 - (1 << PARTITION)),
             (morton.1 - (1 << PARTITION)) | 1 << level,
+        ),
+        (
+            (morton.0 - (1 << PARTITION)),
+            (morton.1 - (1 << PARTITION)),
         ),
     ]
 }
@@ -120,7 +97,10 @@ pub fn south_morton(morton: &Coord) -> [Coord; 2] {
     // child filtered south neighbors; dy := 0
     let level = (morton.0 >> PARTITION) - 1;
     [
-        ((morton.0 - (1 << PARTITION)), (morton.1 - (1 << PARTITION))),
+        (
+            (morton.0 - (1 << PARTITION)),
+            (morton.1 - (1 << PARTITION)),
+        ),
         (
             (morton.0 - (1 << PARTITION)) | 1 << level,
             (morton.1 - (1 << PARTITION)),
@@ -130,7 +110,7 @@ pub fn south_morton(morton: &Coord) -> [Coord; 2] {
 
 fn edge_neighbors(quad: &QuadTree, m_coord: &Coord) -> Vec<Coord> {
     // neighbor and filter need to be opposites ie (neigh east -> filter west);
-    let cardinals = cardinals(m_coord);
+    let cardinals = find_cardinals(m_coord);
     println!("Cardinals {cardinals:?}");
     let filters = [west_morton, south_morton, east_morton, north_morton];
     let level = m_coord.0 >> PARTITION;
@@ -207,25 +187,40 @@ fn astar(quad:&QuadTree, source:&Coord, target:&Coord) -> Vec<Coord> {
 }
 
 fn main() {
-    let x = encode_morton(&(0, 0), 1);
-    assert_eq!((2, 2), point(&x));
-    let x = encode_morton(&(0, 0), 0);
-    assert_eq!((1, 1), point(&x));
+    // point is interface only for distance, does not maintain scale
     let x = encode_morton(&(1, 1), 0);
     assert_eq!((3, 3), point(&x));
+    let x = encode_morton(&(2, 2), 0);
+    assert_eq!((5, 5), point(&x));
+    let x = encode_morton(&(1, 3), 0);
+    assert_eq!((3, 7), point(&x));
+    //-------------
+    let x = encode_morton(&(0, 0), 1);
+    assert_eq!((2, 2), point(&x));
+    let x = encode_morton(&(1, 0), 1);
+    assert_eq!((2, 2), point(&x));
+    let x = encode_morton(&(2, 0), 1);
+    assert_eq!((6, 2), point(&x));
     let x = encode_morton(&(3, 3), 2);
     assert_eq!((4, 4), point(&x));
-    let x = encode_morton(&(3, 0), 1);
-    assert_eq!((6, 2), point(&x));
 
-    let cardinals = cardinals(&(3,3));
-    assert_eq!((5, 3), cardinals[0]);
-    assert_eq!((3, 5), cardinals[1]);
-    assert_eq!((1, 3), cardinals[2]);
-    assert_eq!((3, 1), cardinals[3]);
-    let origin = (1,1);
+    let cardinals = find_cardinals(&(3,3));
+    assert_eq!((4, 3), cardinals[0]);
+    assert_eq!((3, 4), cardinals[1]);
+    assert_eq!((2, 3), cardinals[2]);
+    assert_eq!((3, 2), cardinals[3]);
+
+    let cardinals = find_cardinals(&(2,2));
+    assert_eq!((3, 2), cardinals[0]);
+    assert_eq!((2, 3), cardinals[1]);
+    assert_eq!((1, 2), cardinals[2]);
+    assert_eq!((2, 1), cardinals[3]);
+    let origin = (0,0);
     let m_origin = encode_morton(&origin, 1);;
-    assert_eq!([(5,1), (5,3)], east_morton(&m_origin));
+    assert_eq!([(1,0), (1,1)], east_morton(&m_origin));
+    assert_eq!([(1,1), (0, 1)], north_morton(&m_origin));
+    assert_eq!([(0,1), (0,0)], west_morton(&m_origin));
+    assert_eq!([(0,0), (1,0)], south_morton(&m_origin));
 
     // let source = (3,3);
     // let target = (3,5);
