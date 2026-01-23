0d02db3d37b8b54c300db4667353af86172b4bcb
autumn <cyancirrus@gmail.com>
2026-01-23 17:00:07 -0600

    ensure examples working

--- src/main.rs
+++ src/main.rs
@@ -60,37 +60,3 @@ fn main() {
         }
     }
 }
-
-// fn main() {
-//     println!("------------------------------------");
-//     println!("      Example navigation            ");
-//     println!("------------------------------------");
-//     let path = "./data/sample/test_nav0.map";
-//     match read_grid(path) {
-//         Ok(oracle) => {
-//             let position = (1, 1);
-//             let target = (5, 2);
-//             // let position = (1, 1);
-//             // let target = (18, 3);
-//             // let target = (4, 2);
-//             // let target = (1, 2);
-//             // println!("Test oracle pathclear 0, 1 {:?}", oracle.path_clear(&(1,2)));
-//             let bounds = Bounds::new(0, 0, 32, 32);
-//             let environment = Grid::new(HashMap::new(), bounds);
-//             let lidar = Lidar::new(8, oracle.clone());
-//             let mut sabby = Sabrina::new(position, environment, lidar);
-//             println!("absolute_environment\n{oracle}");
-//             println!("-------------------------------");
-//             println!("    Starting Navigation        ");
-//             println!("-------------------------------");
-
-//             // sabby.scan();
-//             println!("Final Status {:?}", sabby.navigate(target));
-//             println!("Final map\n{}", sabby.environment);
-//             println!("Final bounds\n{:?}", sabby.environment.seen);
-//         }
-//         Err(e) => {
-//             println!("Err\n{e:?}");
-//         }
-//     }
-// }
