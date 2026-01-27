78f579b59b9c0325cbc2ddd52adf283a3a144383
autumn <cyancirrus@gmail.com>
2026-01-26 22:50:40 -0600

    plan unifcation

--- src/main.rs
+++ src/main.rs
@@ -16,25 +16,173 @@ use sabrina::sensor::lidar::Lidar;
 use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::mem;
 use std::time::Instant;
+// use std::slice::Iter;
+
+/// Interface to unite astar and dstarlite traversal
+/// #Members#
+/// * Forward := planner creates nodes from source -> target,
+/// * Backward := planner creates nodes from target -> source,
+
+pub struct ForwardIter<'a> {
+    nodes: &'a [Coord],
+    aug_index: usize,
+}
+
+pub struct BackwardIter<'a> {
+    nodes: &'a [Coord],
+    length: usize,
+    index: usize,
+}
+
+impl <'a> ForwardIter<'a> {
+    pub fn new(nodes:&'a [Coord]) -> Self {
+        Self {
+            nodes,
+            aug_index:nodes.len(),
+        }
+    }
+}
+
+impl <'a> BackwardIter<'a> {
+    pub fn new(nodes:&'a [Coord]) -> Self {
+        Self {
+            nodes,
+            length: nodes.len(),
+            index: 0,
+        }
+    }
+}
+
+impl <'a> Iterator for ForwardIter<'a> {
+    type Item = &'a Coord;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.aug_index > 0 {
+            self.aug_index -= 1;
+            return Some(&self.nodes[self.aug_index])
+        }
+        None
+    }
+}
+
+impl <'a> Iterator for BackwardIter<'a> {
+    type Item = &'a Coord;
+    fn next(&mut self) -> Option<Self::Item> {
+        let index = self.index;
+        if index < self.length {
+            self.index += 1;
+            return Some(&self.nodes[index])
+        }
+        None
+    }
+}
+
+trait PlanIter {
+    fn iter(&self) -> impl Iterator<Item = &Coord>;
+    fn nodes(&self) -> &[Coord];
+}
+
+
+pub struct AStarPlan {
+    plan: Vec<Coord>,
+}
+
+pub struct DStarPlan {
+    plan: Vec<Coord>,
+}
+
+impl PlanIter for AStarPlan {
+    fn nodes(&self) -> &[Coord] {
+        &self.plan
+    }
+    fn iter(&self) -> impl Iterator<Item = &Coord> {
+        ForwardIter::new(self.nodes()) 
+    }
+}
+
+impl PlanIter for DStarPlan {
+    fn nodes(&self) -> &[Coord] {
+        &self.plan
+    }
+    fn iter(&self) -> impl Iterator<Item = &Coord> {
+        BackwardIter::new(self.nodes()) 
+    }
+}
+
+
+
+
+
+
+
+// pub trait Plan {
+    // fn traversal(&self) -> Traversal;
+    // fn nodes(&self) -> &[Coord];
+    // fn iter(&self) -> impl Iterator<Item = &Coord> {
+    //     match self.traversal() {
+    //         Traversal::Forward => self.nodes().iter(),
+    //         Traversal::Backward => self.nodes().iter().rev(),
+    //     }
+    // }
+// }
+
+
+// pub struct DStar {
+//     update_queue: LazyPQueue,
+//     star: Star,
+//     plan: Vec<Coord>
+// }
+
+// impl <'a> Plan for AStar<'a> {
+//     fn nodes(&self) -> &[Coord] {
+//         &self.plan
+//     }
+// }
+
+// impl <'s> Plan for DStar<'s> {
+//     fn nodes(&self) -> &[Coord] {
+//         &self.plan
+//     }
+// }
+
+// // i:= iterator, s:= *'s internal
+// impl <'i, 's> IntoIterator for &'i AStar<'s> {
+//     type Item = &'i Coord;
+//     type IntoIter = Iter<'i, Coord>;
+
+//     fn into_iter(self) -> Self::IntoIter {
+//         self.nodes().iter()
+//     }
+// }
+
+// impl <'i, 's> IntoIterator for &'i DStar<'s> {
+//     type Item = &'i Coord;
+//     type IntoIter = Iter<'i, Coord>;
+
+//     fn into_iter(self) -> Self::IntoIter {
+//         self.nodes().iter().rev()
+//     }
+// }
+
+
+
+
+
+// impl Iterator for Plan {
+// }
 
 fn main() {
-    let levels = 2;
-    let mut map = QuadTree::initialize(levels);
-    map.insert_cell(&(1,1), Belief::Occupied);
-    debug_assert_eq!(map.information.len(), 4);
-    map.insert_cell(&(1,0), Belief::Occupied);
-    debug_assert_eq!(map.information.len(), 4);
-    map.insert_cell(&(0,0), Belief::Occupied);
-    debug_assert_eq!(map.information.len(), 4);
-    map.insert_cell(&(0,1), Belief::Occupied);
-    debug_assert_eq!(map.get_cell(&(1,1)), Some((1, Belief::Occupied)));
-    debug_assert_eq!(map.information.len(), 1);
-    map.insert_cell(&(1,1), Belief::Free);
-    debug_assert_eq!(map.information.len(), 4);
-    map.insert_cell(&(1,0), Belief::Free);
-    map.insert_cell(&(0,0), Belief::Free);
-    map.insert_cell(&(0,1), Belief::Free);
-    debug_assert_eq!(map.information.len(), 1);
-    debug_assert_eq!(map.get_cell(&(1,1)), Some((1, Belief::Free)));
+    let x = vec![(1,1), (2,2), (3,3)];
+    let astar_plan = AStarPlan { plan: x.clone() };
+    let dstar_plan = DStarPlan { plan: x.clone() };
+    
+    println!("AStar plan!");
+    for c in astar_plan.iter() {
+        println!("{c:?}");
+    }
+    println!("DStar plan!");
+    for c in dstar_plan.iter() {
+        println!("{c:?}");
+    }
+
 
 }
