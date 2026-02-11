8b4c3457ae04bac454ff84dc2be272a8385fa37c
autumn <cyancirrus@gmail.com>
2026-02-11 05:42:25 -0600

    siblings done

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,101 @@ use sabrina::parser::grid::read_grid;
 use sabrina::parser::quad::read_quad;
 use sabrina::sensor::lidar::Lidar;
 
-// TODO: Check the point math ensure the scaling by two is still in there
 // TODO: We need to update the vertex for all of the subgrids
 // TODO: think i need to call the update for the grid neighbors as well as edge neighbors
 
 
+fn grid_siblings(node:HCoord) -> [HCoord;4] {
+    // goes in clockwise direction
+    // [bl, br, tr, tl]
+    let h = 1 << node.l;
+    [
+        HCoord {
+            l: node.l,
+            x: node.x & !1,
+            y: node.y & !1,
+        },
+        HCoord {
+            l: node.l,
+            x: node.x | h,
+            y: node.y & !1,
+        },
+        HCoord {
+            l: node.l,
+            x: node.x | h,
+            y: node.y | h,
+        },
+        HCoord {
+            l: node.l,
+            x: node.x & !1,
+            y: node.y | h,
+        },
+    ]
+}
+
+fn test_sibblings() {
+    let x = HCoord { l: 0, x: 0, y: 0 };
+    let eresult = [
+        HCoord { l:0, x: 0, y: 0 },
+        HCoord { l:0, x: 1, y: 0 },
+        HCoord { l:0, x: 1, y: 1 },
+        HCoord { l:0, x: 0, y: 1 },
+    ];
+    assert_eq!(eresult, grid_siblings(x));
+    let x = HCoord { l: 0, x: 1, y: 1 };
+    assert_eq!(eresult, grid_siblings(x));
+    let x = HCoord{l: 2, x: 0, y: 0 };
+    let eresult = [
+        HCoord { l:1, x: 0, y: 0 },
+        HCoord { l:1, x: 4, y: 0 },
+        HCoord { l:1, x: 4, y: 4 },
+        HCoord { l:1, x: 0, y: 4 },
+    ];
+}
+
+fn main() {
+    test_sibblings();
+    // println!("------------------------------------");
+    // println!("      Example navigation            ");
+    // println!("------------------------------------");
+    // let path = "./data/sample/test_nav0.map";
+    // // let path = "./data/sample/test_nav1.map";
+    // let levels = 3;
+    // match (read_quad(path, levels), read_grid(path)) {
+    //     (Ok(q_oracle), Ok(g_oracle)) => {
+    //         // works with levels = 5 for d*lite
+    //         let position = ACoord { x: 1, y: 1 };
+    //         let target = ACoord { x: 1, y: 5 };
+            
+    //         // // TODO: make it work with levels = 3
+    //         let position = ACoord { x: 1, y: 1 };
+    //         let target = ACoord { x: 9, y: 3 };
+
+    //         let position = ACoord { x: 1, y: 1 };
+    //         let target = ACoord { x: 10, y: 4 };
+    //         // works with levels = 2 for d*lite
+    //         // let position = ACoord { x: 3, y: 2 };
+    //         // let target = ACoord { x: 18, y: 3 };
+    //         let environment = QuadTree::init(levels);
+    //         println!("environment\n{:?}", environment);
+    //         // let environment = Grid::new();
+    //         let lidar = Lidar::new(12, g_oracle.clone());
+    //         // let mut sabby = Sabrina::new(position, environment, lidar, BestFirstPlanner);
+    //         // let mut sabby = Sabrina::new(position, environment, lidar, AStarPlanner);
+    //         // let mut sabby = Sabrina::new(position, environment, lidar, DStarPlanner::new());
+    //         let mut sabby = Sabrina::new(position, environment, lidar, DStarPlanner::new());
+    //         println!("absolute_environment\n{q_oracle}");
+    //         // println!("-------------------------------");
+    //         // println!("    Starting Navigation        ");
+    //         // println!("-------------------------------");
+    //         println!("Final Status {:?}", sabby.navigate(target));
+    //         println!("Final map\n{}", sabby.environment);
+    //     }
+    //     _ => {
+    //         println!("Err");
+    //     }
+    // }
+}
 // fn main() {
 //     println!("------------------------------------");
 //     println!("      Example navigation            ");
@@ -35,45 +125,3 @@ use sabrina::sensor::lidar::Lidar;
 //         println!("n {n:?}");
 //     }
 // }
-fn main() {
-    println!("------------------------------------");
-    println!("      Example navigation            ");
-    println!("------------------------------------");
-    let path = "./data/sample/test_nav0.map";
-    // let path = "./data/sample/test_nav1.map";
-    let levels = 2;
-    match (read_quad(path, levels), read_grid(path)) {
-        (Ok(q_oracle), Ok(g_oracle)) => {
-            // works with levels = 5 for d*lite
-            let position = ACoord { x: 1, y: 1 };
-            let target = ACoord { x: 1, y: 5 };
-            
-            // // TODO: make it work with levels = 3
-            let position = ACoord { x: 1, y: 1 };
-            let target = ACoord { x: 9, y: 3 };
-
-            let position = ACoord { x: 1, y: 1 };
-            let target = ACoord { x: 10, y: 4 };
-            // works with levels = 2 for d*lite
-            // let position = ACoord { x: 3, y: 2 };
-            // let target = ACoord { x: 18, y: 3 };
-            let environment = QuadTree::init(levels);
-            println!("environment\n{:?}", environment);
-            // let environment = Grid::new();
-            let lidar = Lidar::new(12, g_oracle.clone());
-            // let mut sabby = Sabrina::new(position, environment, lidar, BestFirstPlanner);
-            // let mut sabby = Sabrina::new(position, environment, lidar, AStarPlanner);
-            // let mut sabby = Sabrina::new(position, environment, lidar, DStarPlanner::new());
-            let mut sabby = Sabrina::new(position, environment, lidar, DStarPlanner::new());
-            println!("absolute_environment\n{q_oracle}");
-            // println!("-------------------------------");
-            // println!("    Starting Navigation        ");
-            // println!("-------------------------------");
-            println!("Final Status {:?}", sabby.navigate(target));
-            println!("Final map\n{}", sabby.environment);
-        }
-        _ => {
-            println!("Err");
-        }
-    }
-}
