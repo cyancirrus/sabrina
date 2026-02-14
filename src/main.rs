1d5435a6099911014a07dc930f9d8afe58bd3709
autumn <cyancirrus@gmail.com>
2026-02-14 13:29:46 -0600

    omg i think that was it, i heavily wow insanely difficult

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,7 @@ fn main() {
     println!("------------------------------------");
     let path = "./data/sample/test_nav0.map";
     // let path = "./data/sample/test_nav1.map";
-    let levels = 3;
+    let levels = 6;
     match (read_quad(path, levels), read_grid(path)) {
         (Ok(q_oracle), Ok(g_oracle)) => {
             // works with levels = 5 for d*lite
@@ -79,8 +79,8 @@ fn main() {
             // let position = ACoord { x: 1, y: 1 };
             // let target = ACoord { x: 15, y: 7 };
             
-            // let position = ACoord { x: 1, y: 1 };
-            // let target = ACoord { x: 15, y: 3 };
+            // // let position = ACoord { x: 1, y: 1 };
+            // // let target = ACoord { x: 15, y: 3 };
 
             // let position = ACoord { x: 1, y: 1 };
             // let target = ACoord { x: 18, y: 3 };
